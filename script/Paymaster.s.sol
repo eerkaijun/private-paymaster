5e97feace7e28f37a6782d4bd9f6e66613494016
eerkaijun <kaijuneer@gmail.com>
2023-05-13 18:14:32 +0100

    mint tokens

--- script/Paymaster.s.sol
+++ script/Paymaster.s.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/MockToken.sol";
 
 contract PaymasterScript is Script {
-    function setUp() public {}
+    MockToken public token;
+
+    function setUp() public {
+        token = MockToken(0xA76AE5A1BE9BCd287e884Cc39C4Dd0EBfecc0E7c);
+    }
 
     function run() public {
-        vm.broadcast();
+        vm.broadcast(vm.envUint("PRIVATE_KEY"));
+        // mint 10 ether to MockMixer
+        token.mint(0xA836380122e58Dff60D3404d8994671b0eF6CCd8, 10 ether);
     }
 }
